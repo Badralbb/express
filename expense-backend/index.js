fdb16e7432a33b4d3894a8eef27b4cca015da0e3
Badralbb <Badralbaigalmaa7@gmail.com>
2024-08-12 19:15:04 +0800

    1

--- expense-backend/index.js
+++ expense-backend/index.js
@@ -3,13 +3,14 @@ const app = express()
 const port = process.env.port || 4000
 const cors = require("cors")
 app.use(cors())
-app.get('/articles/:id', (req, res) => {
-  res.send('Hello World!') 
-  console.log(req.params)
-})
+
 app.get('/articles', (req, res) => {
-    res.json([{title:"Hello world1",id:1},{title:"Hello world2",id:2},{title:"Hello world3",id:3}])
-  })
+  res.json([{ title: "Hello world1", id: 1 }, { title: "Hello world2", id: 2 }, { title: "Hello world3", id: 3 }])
+})
+app.get('/articles/:id', (req, res) => {
+  res.send()
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
