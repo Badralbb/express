fdc2e87560fa3d8859698410efcad424b66d4f8b
Badralbb <Badralbaigalmaa7@gmail.com>
2024-08-12 18:10:44 +0800

    initial

---
+++ expense-backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const cors = require("cors")
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/articles', (req, res) => {
+    res.json([{title:"Hello world1",id:1},{title:"Hello world2",id:2},{title:"Hello world3",id:3}])
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ expense-web/app/page.js
@@ -0,0 +1,30 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [articles,setArticles] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:4000/articles").then(res => res.json()).then(data=>{
+      setArticles(data)
+    })
+  })
+  return (
+    <main>
+
+      <div>
+        {
+            articles.map(article => (
+              <div className="bg-blue-500" key={article.id}>
+                {
+                  article.title
+                }
+
+              </div>
+            ))
+        }
+      </div>
+
+    </main>
+  );
+}
