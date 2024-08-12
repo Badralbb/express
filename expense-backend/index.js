8853c77c2ed2bbd3f368e99bca185c9f4a69457a
Badralbb <Badralbaigalmaa7@gmail.com>
2024-08-12 19:44:35 +0800

    1

--- expense-backend/index.js
+++ expense-backend/index.js
@@ -8,7 +8,7 @@ app.get('/articles', (req, res) => {
   res.json([{ title: "Hello world1", id: 1 }, { title: "Hello world2", id: 2 }, { title: "Hello world3", id: 3 }])
 })
 app.get('/articles/:id', (req, res) => {
-  res.send()
+  res.send("Hello world")
 })
 
 
