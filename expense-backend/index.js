51574d71f927051361cf2ba170b63f5865dfc26e
Badralbb <Badralbaigalmaa7@gmail.com>
2024-08-13 18:04:01 +0800

    1

--- expense-backend/index.js
+++ expense-backend/index.js
@@ -8,23 +8,23 @@ app.get('/articles', (req, res) => {
   res.json([{ title: "Hello world1", id: 1 }, { title: "Hello world2", id: 2 }, { title: "Hello world3", id: 3 }])
 })
 
- const content = fs.readFileSync("content.json","utf-8")
- 
+const content = fs.readFileSync("content.json", "utf-8")
+
 console.log(content)
 const categories = JSON.parse(content)
 
 
-app.get("/categories/list",(req,res)=>{
+app.get("/categories/list", (req, res) => {
   res.json(categories)
 })
 
-app.get("/categories/create",(req,res)=>{
+app.get("/categories/create", (req, res) => {
 
   const name = req.query.name
- categories.push({name: name})
+  categories.push({ name: name })
 
- fs.writeFileSync("content.json",JSON.stringify(categories))
- res.json(["Success"])
+  fs.writeFileSync("content.json", JSON.stringify(categories))
+  res.json(["Success"])
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- expense-web/app/page.js
+++ expense-web/app/page.js
@@ -5,41 +5,40 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [categories, setCategories] = useState([])
 
-  function loadList() {
-    fetch("http://localhost:4000/categories/list").then(res => res.json()).then(data => {
-      setCategories(data)
-    })
+  const loadList = async () => {
+    const response = await fetch("http://localhost:4000/categories/list")
+    const data = await response.json();
+    setCategories(data)
   }
-  useEffect(() => {
-    loadList()
-  }, [])
-  function createNewCategory() {
-    const name = prompt("name...")
-    fetch(`http://localhost:4000/categories/create?name=${name}`).then(res => {
-      res.json()
-    }).then(() => {
 
-      loadList()
-    })
-  }
-  return (
-    <main>
+useEffect(() => {
+  loadList()
+}, [])
+const createNewCategory = async () => {
+  const name = prompt("name...")
+  const response = await fetch(`http://localhost:4000/categories/create?name=${name}`)
+  const data = await response.json()
+  loadList()
 
-      <div>
+}
+return (
+  <main>
 
-        <button onClick={createNewCategory} className="bg-red-500 rounded-2xl w-52">Add New</button>
-        {
-          categories.map(category => (
-            <div className="bg-blue-500" key={category.name}>
-              {
-                category.name
-              }
+    <div>
 
-            </div>
-          ))
-        }
-      </div>
+      <button onClick={createNewCategory} className="bg-red-500 rounded-2xl w-52">Add New</button>
+      {
+        categories.map(category => (
+          <div className="bg-blue-500" key={category.name}>
+            {
+              category.name
+            }
 
-    </main>
-  );
+          </div>
+        ))
+      }
+    </div>
+
+  </main>
+);
 }
