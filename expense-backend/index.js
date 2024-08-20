cda29c9207d5a25af0b16790227142b80237a1fc
Badralbb <Badralbaigalmaa7@gmail.com>
2024-08-20 16:59:43 +0800

    1

--- expense-backend/index.js
+++ expense-backend/index.js
@@ -1,48 +1,24 @@
 
 const {startApp} = require("./configs/basic")
-const fs = require("fs")
+const { getCategory, getCategories, createNewCategory, deleteCategory, updateCategories } = require("./services/categoryService")
 
-const { v4: uuidv4 } = require('uuid');
+
 
 
 const app = startApp()
 
-const getCategory = ({id})=>{
-  const category = categories.find(cat => cat.id === id)
-  return category
-}
 
-const deleteCategory = ({id}) =>{
-  categories = categories.filter((cat) => cat.id != id)
-  fs.writeFileSync("data/content.json", JSON.stringify(categories))
-}
 
-const updateCategories = ({id,updatedName}) =>{
-  const index = categories.findIndex(cat => cat.id == id)
-
-  categories[index].name = updatedName
-
-  fs.writeFileSync("data/content.json", JSON.stringify(categories))
-}
-
-const createNewCategory = async (form) =>{
-  const id = uuidv4()
-  form.id = id
-  categories.push(form)
-
-  fs.writeFileSync("data/content.json", JSON.stringify(categories))
-  return id
-} 
-
-let categories = JSON.parse(fs.readFileSync("data/content.json", "utf-8"))
 
 app.get("/categories", (req, res) => {
+  const categories = getCategories();
   res.json(categories)
 })
 app.get("/categories/:id", (req, res) => {
   const { id } = req.params
   const category = getCategory({id})
-  res.sendStatus(200)
+  console.log(category)
+  res.json(category)
 })
 
 app.post("/categories", async (req, res) => {
---
+++ expense-backend/services/categoryService.js
@@ -0,0 +1,44 @@
+const fs = require("fs")
+
+const { v4: uuidv4 } = require('uuid');
+
+const getCategories = () =>{
+    const categories = JSON.parse(fs.readFileSync("data/content.json", "utf-8"))
+    return categories
+}
+const getCategory = ({ id }) => {
+
+    const category = categories.find(cat => cat.id === id)
+    return category
+}
+
+const deleteCategory = ({ id }) => {
+    categories = categories.filter((cat) => cat.id != id)
+    fs.writeFileSync("data/content.json", JSON.stringify(categories))
+}
+
+const updateCategories = ({ id, updatedName }) => {
+    const index = categories.findIndex(cat => cat.id == id)
+
+    categories[index].name = updatedName
+
+    fs.writeFileSync("data/content.json", JSON.stringify(categories))
+}
+
+const createNewCategory = async (form) => {
+    const id = uuidv4()
+    form.id = id
+    categories.push(form)
+
+    fs.writeFileSync("data/content.json", JSON.stringify(categories))
+    return id
+}
+
+
+module.exports = {
+    getCategory,
+    deleteCategory,
+    updateCategories,
+    createNewCategory,
+    getCategories,
+}
